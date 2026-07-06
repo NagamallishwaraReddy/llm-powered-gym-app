2b573886b10de1d4420497f227fcd1b4e18bdf55
NagamallishwaraReddy <nagamalleswarareddyyeruva@gmail.com>
2026-07-06 23:03:26 +0530

    new project

---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import { profileRouter } from "./routes/profile";
+import { planRouter } from "./routes/plan";
+// import { profileRouter } from "./routes/profile";
+// import { planRouter } from "./routes/plan";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+//API Routes
+app.use("/api/profile", profileRouter);
+app.use("/api/plan", planRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
